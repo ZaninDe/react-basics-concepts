9bc24655ad7fae0f9d0fc7378b7b5407891d42f0
ZaninDe <gabriel.zaninde@gmail.com>
2022-03-06 23:26:08 -0300

    added input

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,24 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import './styles.css';
 
-
-
 export const Button = () => {
   const [count, setcount] = useState(0);
-
+ 
   useEffect(() => {
-    console.log('a sample useState and useEffect example')
-  },[])
+    console.log('a sample useState and useEffect example');
+  }, []);
+
+   const sum = document.getElementById('value');
 
   return (
     <>
-      <button className="button" onClick={() => setcount(count + 1)}>
-        <span>{count}</span>
-      </button>
+      <input id="value" type="text" />
 
-      <button className="button" onClick={() => setcount(0)}>
-        <span>clear</span>
-      </button>
+      <div>
+        <button
+          className="button"
+          onClick={(() => setcount(count + 1), console.log(sum))}
+        >
+          <span>{count}</span>
+        </button>
+
+        <button className="button" onClick={() => setcount(0)}>
+          <span>clear</span>
+        </button>
+      </div>
     </>
   );
 };

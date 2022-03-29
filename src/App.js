b9be7782ee0e8c523ef6f9ef0dfa44f09be5b73a
ZaninDe <gabriel.zaninde@gmail.com>
2022-03-28 23:10:19 -0300

    button component refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { Button } from './components/Button';
-import { Header } from './components/Header';
-import './global.css';
 
 export default function App() {
   return (
-    <div>
       <>
         <Button />
       </>
-    </div>
-  );
+  )
 }
